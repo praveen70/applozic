9df741df72d341665e3ef77b39ff5ddb60f7455b
praveen70 <praveen.mr288@gmail.com>
2020-07-13 23:52:12 +0530

    initial commit

---
+++ build/precache-manifest.9f8b6da014bbd41e571778aca788335f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4dd8d7fea2e1c42bd0ca87515c0545",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1ef1ecbc4fcd1ab1802",
+    "url": "/static/css/main.937952ce.chunk.css"
+  },
+  {
+    "revision": "4e358ae87842ce87e2e4",
+    "url": "/static/js/2.73dc9d43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.73dc9d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ef1ecbc4fcd1ab1802",
+    "url": "/static/js/main.e57b4c2a.chunk.js"
+  },
+  {
+    "revision": "2eb31a2e3ef446205ce8",
+    "url": "/static/js/runtime-main.bd665d2e.js"
+  }
+]);
